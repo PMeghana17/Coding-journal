c1142efee7b00b8cbd1a1c9251c097733227c947
Meghana <142651355+PMeghana17@users.noreply.github.com>
2025-04-29 22:18:27 +0530

    Arrays

--- Day 1/1].Largest ELement in Array.java
+++ Day 1/1].Largest ELement in Array.java
@@ -15,7 +15,17 @@ public class tuf {
   }
 }
 
-
+--------------------------------------------------
+Method 2 
+ {
+ public static void main(String args[]) {
+  int max=Integer.MIN_VALUE;
+  for(int i=0;i<nums.length;i++)
+   {
+    max=Math.max(max,nums[i]);
+    }
+ }
+}
 Output:
 
 The Largest element in the array is: 5
