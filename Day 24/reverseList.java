0fe4238751b9db35bb89586c7fb9f0c3bed87053
Meghana <142651355+PMeghana17@users.noreply.github.com>
2025-06-07 22:11:14 +0530

    Tutort LL

---
+++ Day 24/reverseList.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+        Stack<ListNode> stack=new Stack<ListNode>();
+
+        while(curr!=null)
+        {
+            stack.push(curr);
+            curr=curr.next;
+        }
+
+        ListNode res=new ListNode(-1);
+        curr=res;
+
+        while(!stack.isEmpty())
+        {
+            curr.next=stack.pop();
+            curr=curr.next;
+        }
+
+        curr.next=null;
+
+        return res.next;
+
+    }
+}
+TC:O(N)
+SC:O(N)
