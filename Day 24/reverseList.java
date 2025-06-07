ca7e25a37fe3ad75e577c46d610414e156214613
Meghana <142651355+PMeghana17@users.noreply.github.com>
2025-06-07 22:12:42 +0530

    Tutort LL

--- Day 24/reverseList.java
+++ Day 24/reverseList.java
@@ -1,3 +1,6 @@
+LC---[206]
+ Reverse Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list.
 /**
  * Definition for singly-linked list.
  * public class ListNode {
